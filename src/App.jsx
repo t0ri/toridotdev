349d72a5a03e8c96ff75672589aea362beef5100
t0ri <victoriarosemurray@gmail.com>
2020-01-07 11:37:18 -0800

    add comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,21 @@ import Articles from './Articles/Articles.jsx'
 
 import './App.css'
 
+
+// SiteRouter Function Component
+// Returns component that applies to current route
+// Displays at HTML Root
 function SiteRouter() {
   return (
     <Router>
       <div>
+        {/* Checks Route, Returns Component */}
         <Route path="/" exact component={Index} />
+        <Route path="/projects" component={Project} />
         <Route path="/projects/:id" component={Project} />
         <Route path="/articles" component={Articles} />
-
+        
+        {/* Renders Navigation Component */}
         <Navigation />
       </div>
     </Router>
