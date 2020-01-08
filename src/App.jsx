ea3a6f2b2da1efdae6c8e4a1e48574fb0bd8dce6
t0ri <victoriarosemurray@gmail.com>
2020-01-08 15:01:50 -0800

    update refs for sass

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navigation from './Navigation/Navigation'
 import Index from './Index/Index'
 import Projects from './Projects/Projects'
-import Articles from './Articles/Articles.jsx'
+import Articles from './Articles/Articles'
 
-import './App.css'
+import './App.scss'
 
 
 // SiteRouter Function Component
@@ -23,7 +23,7 @@ function SiteRouter() {
         <Route path="/projects" component={Projects} />
         <Route path="/projects/:id" component={Projects} />
         <Route path="/articles" component={Articles} />
-        
+
         {/* Renders Navigation Component */}
         <Navigation />
       </div>
