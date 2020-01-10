821e1985ae41425b702f425a80a49bf3019a2e40
t0ri <victoriarosemurray@gmail.com>
2020-01-09 16:32:33 -0800

    card final touches

--- src/Projects/ProjectCard.jsx
+++ src/Projects/ProjectCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 export default class ProjectCard extends React.Component {
   constructor(props) {
