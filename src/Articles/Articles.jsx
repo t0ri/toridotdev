2e71976c3f196d5ab9a3fa680fe0a7e9c2f295d2
t0ri <victoriarosemurray@gmail.com>
2020-01-09 16:24:48 -0800

    update article styling

--- src/Articles/Articles.jsx
+++ src/Articles/Articles.jsx
@@ -4,82 +4,8 @@ import React, { Component } from 'react'
 // Get article JSON from file
 import data from './data'
 
+import Article from './ArticleCard'
 import './articles.scss'
-
-// Articles Class Component
-// Returns a Card containing an Article
-// Rendered by Articles.getArticles()
-class Article extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isFocused: false,
-    }
-
-    // Binding `this`
-    this.handleFocus = this.handleFocus.bind(this)
-  }
-
-  handleFocus() {
-    this.setState((state) => ({
-      isFocused:
-        !state.isFocused
-    }))
-  }
-
-  // eslint-disable-next-line consistent-return
-  focusCard() {
-    // Destructuring state
-    const { isFocused } = this.state
-
-    if (isFocused === true) {
-      return 'focus'
-    }
-  }
-
-  // eslint-disable-next-line consistent-return
-  focusUnderline() {
-    const { isFocused } = this.state
-
-    if (isFocused === true) {
-      return 'underline'
-    }
-  }
-
-  // Return Article Card
-  render() {
-    // Destructuring props
-    const { article } = this.props
-    const {
-      title,
-      description,
-      link,
-      img,
-    } = article
-
-    return (
-      <div
-        className={`card hvr-grow ${this.focusCard()}`}
-        onMouseEnter={this.handleFocus}
-        onMouseLeave={this.handleFocus}
-      >
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          alt={title}
-        >
-          <div className="top-crop">
-            <img src={img} alt={title} />
-          </div>
-          <h2><span className={this.focusUnderline()}>{title}</span></h2>
-        </a>
-        <p>{description}</p>
-      </div>
-    )
-  }
-}
 
 
 // Articles Class Component
