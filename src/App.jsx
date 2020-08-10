d6d8e47cc5b26204587138aa9460f5f4a6522d99
t0ri <victoriarosemurray@gmail.com>
2020-08-10 16:58:54 -0700

    update to class component

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,50 @@ import Articles from './Articles/Articles'
 import './App.scss'
 
 
-// SiteRouter Function Component
+// SiteRouter Class Component
 // Returns component that applies to current route
 // Displays at HTML Root
-function SiteRouter() {
-  // Set Web Page Title
-  document.title = '@t0ri\'s website'
-  return (
-    <Router>
-      <div id="SiteRouter">
-        {/* Checks Route, Returns Component */}
-        <Route path="/" exact component={Index} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/projects/:id" component={Projects} />
-        <Route path="/articles" component={Articles} />
 
-        {/* Renders Navigation Component */}
-        <Navigation />
-      </div>
-    </Router>
-  )
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      projects: undefined,
+    }
+
+    // Populate `this.state.projects`
+    this.getProjects()
+  }
+
+  async getProjects() {
+    // Fetch from API and push data into `list`
+    // Set `this.state.projects` to call render() again
+    const response = await fetch('https://cors-anywhere.herokuapp.com/https://toridotdev-api.herokuapp.com/projects')
+    const data = await response.json()
+    this.setState({ projects: await data })
+  }
+
+  render() {
+    // Deconstructing `this.state`
+    const { projects } = this.state
+
+    // Set Web Page Title
+    document.title = '@t0ri\'s website'
+
+    return (
+      <Router>
+        <div id="SiteRouter">
+          {/* Checks Route, Returns Component */}
+          <Route path="/" exact component={Index} />
+          <Route path="/projects" render={() => <Projects projects={projects} />} />
+          <Route path="/projects/:id" component={Projects} />
+          <Route path="/articles" component={Articles} />
+
+          {/* Renders Navigation Component */}
+          <Navigation />
+        </div>
+      </Router>
+    )
+  }
 }
-
-export default SiteRouter
