06ea9a6734a7b659394bbcc6a2212433ca1de231
tor <tor@tors-MacBook-Pro.local>
2021-08-19 15:03:32 -0700

    remove AnimatedBackground from rendering

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -144,7 +144,7 @@ export default class Projects extends Component {
             }
           </div>
         </div>
-        <AnimatedBackground />
+        {/* <AnimatedBackground /> */}
       </section>
     )
   }
